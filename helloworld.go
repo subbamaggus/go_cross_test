5df4f6ca6d4beddaa88c806ba810bb9958a716a1
Markus Kerle <m.kerle@jeanmueller.de>
2022-06-25 09:34:59 +0200

    first external

--- helloworld.go
+++ helloworld.go
@@ -2,9 +2,12 @@ package main
 
 import (
     "fmt"
+    "mymod/mytestlib"
 )
 
 func main() {
     fmt.Println("hello world")
+    result := mytestlib.MySum(1,2)
+    fmt.Println("result: %d", result)
     fmt.Scanln()
 }    
---
+++ mytestlib/mysum.go
@@ -0,0 +1,5 @@
+package mytestlib
+
+func MySum(a int, b int) int {
+    return a + b
+}
